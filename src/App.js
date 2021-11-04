b34d6baa41f7683fe89d187a487f121d6451930b
thompsian <thompsian@gmail.com>
2021-11-04 15:25:22 -0700

    add timer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 function App() {
     const [text, setText] = useState("")
+    const [timeRemaining, setTimeRemaining] = useState(5)
     
     function handleChange(e) {
         const {value} = e.target
@@ -13,6 +14,14 @@ function App() {
         return wordsArr.filter(word => word !== "").length
     }
     
+    useEffect(() => {
+        if(timeRemaining > 0) {
+            setTimeout(() => {
+                setTimeRemaining(time => time - 1)
+            }, 1000)
+        }
+    }, [timeRemaining])
+    
     return (
         <div>
             <h1>How fast do you type?</h1>
@@ -20,8 +29,8 @@ function App() {
                 onChange={handleChange}
                 value={text}
             />
-            <h4>Time remaining: ???</h4>
-            <button onClick={() => calculateWordCount(text)}>Start</button>
+            <h4>Time remaining: {timeRemaining}</h4>
+            <button onClick={() => console.log(calculateWordCount(text))}>Start</button>
             <h1>Word count: ???</h1>
         </div>
     )
